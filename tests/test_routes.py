aa20d478fc513f5a55ec287ff7db2917157fb7a3
treymurray01 <treymurray13@outlook.com>
2022-12-23 17:44:13 -0500

    lint fixes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -254,4 +254,3 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-        
